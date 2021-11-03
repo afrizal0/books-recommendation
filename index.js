885bca8a7920e5319d28b05b76e908b1de6c5b41
afrizal0 <wikimedicx4504@gmail.com>
2021-11-03 19:47:38 +0800

    create home.html, refactor code

--- index.js
+++ index.js
@@ -3,17 +3,18 @@ const twig = require('twig')
 const bodyParser = require('body-parser')
 const app = express()
 const connection = require('./config/database.js')
-const fileUpload = require('express-fileupload')
 
-app.set('view engine','html');
+app.set('view engine','ejs');
 app.engine('html', twig.__express);
 app.set('views','views');
-
-app.use(fileUpload());
+app.use(express.static(__dirname + '/views'));
 
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.get('/', (req, res) => {
+	res.render('home')
+})
+app.get('/home', (req, res) => {
 	connection.query('SELECT * FROM books', (err, results) => {
 		if(err) throw err;
 		else{
@@ -30,18 +31,14 @@ app.get('/post', (req, res) => {
 
 // POST DATA TO MYSQL DB
 app.post('/post', (req, res) => {
-	
 
 	//Get data from ./views/post.html
-	
-	console.log(req.files)
-
 	const username = req.body.username
 	const book_name = req.body.book_name
 	const description = req.body.description
 
 	const posts = {
-		book_name: book_name,
+		bookname: book_name,
 		username: username,
 		description: description
 	}
@@ -50,18 +47,15 @@ app.post('/post', (req, res) => {
 	connection.query('INSERT INTO books SET?', posts, (err) => {
 		if(err) {
 			console.log('error')
-			throw err;
-			
+			throw err;			
 		}
 		console.log('Data Inserted')
+		res.redirect('/home')
 	})
-	res.redirect('/')
+	
 })
-
-
 
 connection.connect((err) => {
 	if(err) throw err;
 	app.listen(3000)
 })
-
